bd924360b5ba89d7c0121d87407b603e6ab9de3a
thw <thw@thw-a2>
2021-04-28 15:25:55 +0200

    héritage validator

--- src/scripts/js/cetcal/classes/cetcal.class.formvalidator.js
+++ src/scripts/js/cetcal/classes/cetcal.class.formvalidator.js
@@ -1,9 +1,18 @@
-class FormValidator {
+class Validator {
+
     constructor(form) {
-        this.form = form;
-        /*
-                this.mandatoryInputs = mandatoryInputs;
-        */
+       this.form = form;
+    }
+
+    validateFields(field) { 
+
+      // vérifie la présence de valeur
+      if (field.value.trim() === "") {
+          this.setStatus(field, `${field.previousElementSibling.innerText} ce champ ne peut pas être vide`, "error");
+      } else {
+          console.log(field.value);
+          this.setStatus(field, null, "success");
+      }
     }
 
     initialize() {
@@ -26,6 +35,40 @@ class FormValidator {
         return fields;
     }
 
+    // validate on entry
+    validateOnEntry() {
+        let self = this;
+        const fields = self.getFieldsOfForm();
+        fields.forEach(field => {
+            const input = document.querySelector(`#${field}`);
+            input.addEventListener('change', event => {
+                self.validateFields(input);
+            });
+        })
+    }
+
+    setStatus(field, message, status) {
+        const errorMessage = field.parentElement.querySelector('.error-message')
+
+        // selectionner les icones erreurs
+        if (status === "success") {
+            field.classList.add('is-valid');
+            field.classList.remove('is-invalid');
+            return status;
+        }
+
+        if (status === "error") {
+            field.classList.add('is-invalid');
+            field.classList.remove('is-valid');
+            field.parentElement.querySelector('.error-message').innerText = message;
+        }
+
+    }
+
+}
+
+class FormValidator extends Validator {
+
     /*    //mandatory Inputs
 
         mandatoryChecker(){
@@ -39,27 +82,9 @@ class FormValidator {
 
     // validate on submit
 
-    // validate on entry
-
-    validateOnEntry() {
-        let self = this;
-        const fields = self.getFieldsOfForm();
-        fields.forEach(field => {
-            const input = document.querySelector(`#${field}`);
-            input.addEventListener('change', event => {
-                self.validateFields(input);
-            })
-        })
-    }
-
     validateFields(field) {
-        // vérifie la présence de valeur
-        if (field.value.trim() === "") {
-            this.setStatus(field, `${field.previousElementSibling.innerText} ce champ ne peut pas être vide`, "error");
-        } else {
-            console.log(field.value);
-            this.setStatus(field, null, "success");
-        }
+        super.validateFields(field);
+        
         // controle de la validité des heures
         if (field.id === "timeInput-heure-fin" || field.id === "timeInput-heure-deb") {
             const heureDeb = this.form.querySelector('#timeInput-heure-deb');
@@ -76,26 +101,6 @@ class FormValidator {
                 this.setStatus(field, null, "success");
             }
         }
-
-
-    }
-
-
-    setStatus(field, message, status) {
-        const errorMessage = field.parentElement.querySelector('.error-message')
-        // selectionner les icones erreurs
-        if (status === "success") {
-            field.classList.add('is-valid');
-            field.classList.remove('is-invalid');
-            return status
-        }
-
-        if (status === "error") {
-            field.classList.add('is-invalid');
-            field.classList.remove('is-valid');
-            field.parentElement.querySelector('.error-message').innerText = message;
-        }
-
     }
 
 }
