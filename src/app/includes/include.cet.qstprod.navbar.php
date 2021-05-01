e80a8f96a6ee1be69023c2d5bccaef966c803527
thw <thw@thw-a2>
2021-05-01 19:10:21 +0200

    more work on navbar.

--- src/app/includes/include.cet.qstprod.navbar.php
+++ src/app/includes/include.cet.qstprod.navbar.php
@@ -1,12 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/?" style="color: #009c31 !important; font-family: 'Signika' !important; font-weight: 
-  bold;">
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
+  <a class="navbar-brand" href="/?">
     DECIDELABIOLOCALE.ORG
   </a>
-  <button class="navbar-toggler margin-80-infeq-700" id="cet-navbar-hamburger" type="button" data-toggle="collapse" 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" 
     data-target=".navbar-cet-qstprod" aria-controls="navbar-cet-qstprod" 
     aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <span class="cet-navbar-toggler"><i class="fas fa-bars"></i></span>
   </button>
   <div class="navbar-collapse collapse w-100 navbar-cet-qstprod">
     <ul class="navbar-nav mr-auto">
@@ -17,46 +16,66 @@
         </li>
       <?php endif; ?>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle cet-p" href="#" id="navbar-qui-nous-sommes-dropdown" 
+        <a class="nav-link dropdown-toggle cet-p-20" href="#" id="navbar-qui-nous-sommes-dropdown" 
           role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Qui sommes nous ?    
+          &#160;Qui sommes nous ?    
         </a>
         <div class="dropdown-menu" aria-labelledby="navbar-qui-nous-sommes-dropdown">
-          <a class="dropdown-item" href="http://castillonnaisentransition.org/" target="_blank">
+          <a class="dropdown-item cet-p-20" href="http://castillonnaisentransition.org/" target="_blank">
             - L'association CET, à propos
           </a>
-          <a class="dropdown-item" href="https://github.com/j-fish/cetcal" target="_blank">
+          <a class="dropdown-item cet-p-20" href="https://github.com/j-fish/cetcal" target="_blank">
             - Notre projet sur GitHub
           </a>
         </div>
       </li>
-      <li class="nav-item">
-        <a id="cet-notre-projet" class="nav-link cet-p" href="#"
-          style="white-space: nowrap !important;">
-          <i class="fas fa-info-circle"></i>&#160;&#160;Notre projet décidelabiolocale.org
-        </a>
-      </li>
-      <!-- PARTENAIRES -->
-      <li class="nav-item">
-        <a class="nav-link cet-p" href="/?statut=partenaires.liens&anr=true">
-          <i class="fas fa-hands-helping"></i>&#160;&#160;&#160;Partenaires et liens utiles
-        </a>
-      </li>
     </ul>
+
+    <ul class="navbar-nav ml-auto">
+      <?php if ($OPEN_LOGIN_SIGNUP && !$cnx_done && in_array($statut, NavbarHelper::$status_connection_signup)): ?>
+        <li class="nav-item">
+          <a id="cet-annuaire-user-login" class="btn cet-navbar-btn cet-navbar-btn-small" href="#"
+            onmousedown="$('#cet-qstprod_seconnecter').show('slow', function(){ 
+              $('#cetcal-cnx-not-done').hide('slow'); 
+              $('#cetcal-obl-done').hide('slow'); 
+              $('#cetcal-obl-not-done').hide('slow'); });">
+            <i class="fas fa-user"></i>&#160;&#160;Se connecter
+          </a>
+        </li>
+        <li class="nav-item">
+          <a id="cet-annuaire-user-signup" class="btn cet-navbar-btn cet-navbar-btn-small" 
+            href="/?statut=user.signup&anr=true">
+            <i class="fas fa-info-circle"></i>&#160;&#160;Inscription (non producteur.e)
+          </a>
+        </li>
+      <?php endif; ?>
+    </ul>
+
   </div>
 </nav>
 <?php if (!in_array($statut, CetQstProdFilArianneHelper::$states)): ?>
   <?php include $PHP_INCLUDES_PATH.'navbar-entities/include.cet.qstprod.nav.leftpanel.php'; ?>
 <?php endif; ?>
-<div class="row justify-content-around">
-  <div class="col-1 hidden-infeq-700"></div>
-  <div id="w-100-infeq-700-div-id" class="col-8">
-    <img class="img-fluid" src="/res/content/DCDL_biolocale_2.jpg" alt="">
+
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
+  <div class="row justify-content-between">
+    <div id="w-100-infeq-700-div-id" class="col-6">
+      <img class="img-fluid" src="/res/content/DCDL_biolocale_2.jpg" alt="">
+    </div>
+    <div class="col-6 my-top hidden-infeq-700">
+      <div class="row">
+        <div class="col-4 hidden-infeq-700">
+        </div> 
+        <div class="col-4 hidden-infeq-700">
+          <img class="img-fluid float-right" src="/res/content/partenaires/logo_gironde.jpg" height="80">
+        </div>
+        <div class="col-2 hidden-infeq-700">
+          <img class="img-fluid float-right" src="/res/content/partenaires/logo_region.jpg" height="80">
+        </div>
+        <div class="col-2 hidden-infeq-700">
+          <img class="img-fluid float-right" src="/res/content/partenaires/logo_FSE.jpg" height="80">
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="col-2 my-auto hidden-infeq-700" align="center">
-    <img class="float-left" src="/res/content/partenaires/logo_region.jpg" height="80">
-    <img class="float-left" src="/res/content/partenaires/logo_gironde.jpg" height="80">
-    <img class="float-left" src="/res/content/partenaires/logo_FSE.jpg" height="80">
-  </div>
-  <div class="col-1 hidden-infeq-700"></div>
-</div>
+</nav>
--- src/app/includes/navbar-entities/include.cet.qstprod.nav.leftpanel.php
+++ src/app/includes/navbar-entities/include.cet.qstprod.nav.leftpanel.php
@@ -1,45 +1,40 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
   <div class="navbar-collapse collapse w-100 order-3 dual-collapse2 navbar-cet-qstprod">
     <ul class="navbar-nav"> 
       <!-- Lieux de vente -->
       <li class="nav-item" style="margin-right: 4px;">
-        <a class="nav-link cet-p" href="/?statut=asso.vente&anr=true&type=">
-          <i class="fa fa-book" aria-hidden="true"></i>&#160;&#160;&#160;Lieux de vente
+        <a class="nav-link cet-p-20" href="/?statut=asso.vente&anr=true&type=">
+          <i class="fa fa-book" aria-hidden="true"></i>&#160;&#160;Lieux de vente
         </a>
       </li>
       <!-- listing prd complet -->
       <li class="nav-item" style="margin-right: 4px;">
-        <a class="nav-link cet-p" id="cet-annuaire-crt-recherche-btn" 
+        <a class="nav-link cet-p-20" id="cet-annuaire-crt-recherche-btn" 
           href="/?statut=listing.producteurs&anr=true">
-          <i class="fas fa-seedling"></i>&#160;&#160;&#160;Je souhaite consommer BIO local 
+          <i class="fas fa-seedling"></i>&#160;&#160;Je souhaite consommer BIO local 
         </a>
       </li>
       <?php if ($OPEN_PAGE_RECETTES): ?>
         <li class="nav-item" style="margin-right: 4px;">
-          <a class="nav-link cet-p" href="/?statut=base.recettes&anr=true">
-            <i class="fas fa-temperature-high"></i>&#160;&#160;&#160;Livret de recettes
+          <a class="nav-link cet-p-20" href="/?statut=base.recettes&anr=true">
+            <i class="fas fa-temperature-high"></i>&#160;&#160;Livret de recettes
           </a>
         </li>
       <?php endif; ?>
     </ul>
     <ul class="navbar-nav ml-auto">
-      <?php if ($OPEN_LOGIN_SIGNUP && !$cnx_done && in_array($statut, NavbarHelper::$status_connection_signup)): ?>
-        <li class="nav-item" style="margin-right: 4px;">
-          <a id="cet-annuaire-user-login" class="btn cet-navbar-btn" href="#"
-            onmousedown="$('#cet-qstprod_seconnecter').show('slow', function(){ 
-              $('#cetcal-cnx-not-done').hide('slow'); 
-              $('#cetcal-obl-done').hide('slow'); 
-              $('#cetcal-obl-not-done').hide('slow'); });">
-            <i class="fas fa-user"></i>&#160;Se connecter
-          </a>
-        </li>
-        <li class="nav-item">
-          <a id="cet-annuaire-user-signup" class="btn cet-navbar-btn" 
-            href="/?statut=user.signup&anr=true">
-            <i class="fas fa-info-circle"></i>&#160;Inscription (non producteur.e)
-          </a>
-        </li>
-      <?php endif; ?>
+      <li class="nav-item">
+        <a id="cet-notre-projet" class="nav-link cet-p-20" href="#"
+          style="white-space: nowrap !important;">
+          <i class="fas fa-info-circle"></i>&#160;&#160;Notre projet décidelabiolocale.org
+        </a>
+      </li>
+      <!-- PARTENAIRES -->
+      <li class="nav-item">
+        <a class="nav-link cet-p-20" href="/?statut=partenaires.liens&anr=true">
+          <i class="fas fa-hands-helping"></i>&#160;&#160;Partenaires et liens utiles
+        </a>
+      </li>
     </ul>
   </div>
 </nav>
