6445e489ff110a9389026ccb6a20f782a5f969ef
thw <thw@thw-a2>
2021-05-14 12:55:40 +0200

    attention simples quotes...

--- src/app/const/cet.annuaire.const.types.php
+++ src/app/const/cet.annuaire.const.types.php
@@ -16,7 +16,7 @@ Class CetAnnuaireConstTypes
    * à gauche les code_type ou code_sous_type de la table cetcal_type_lieu, à droite le cetcal_entite.type.
    */
   const TRANSCO_TYPE_ENTITE = [
-    "mprd" => "",
+    "mprd" => "''",
     "mbio" => "'mbio'",
     "amap" => "'amap'",
     "rvcc" => "'amap'",
