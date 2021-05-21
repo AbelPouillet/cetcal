902e6b30497a589b444bd55b7d69eaef5ec5a4a4
paulbis <tfydRuB2QCh33yH>
2021-05-21 14:42:29 +0200

    responsive footer

--- src/app/includes/include.cet.qstprod.footer.php
+++ src/app/includes/include.cet.qstprod.footer.php
@@ -1,8 +1,8 @@
 <footer class="cet-footer" id="cet-annuaire-footer" name="cet-annuaire-footer" 
   style="color: rgb(30,40,30) !important; font-family: 'Signika' !important;">
-  <div class="container">
-    <div class="row">
-      <div class="col-6">
+  <div class="container-fluid">
+    <div class="row flex-lg-row flex-column-reverse d-flex">
+      <div class="col-12 col-xl-4 offset-xl-2">
         <ul class="list-group list-group-flush">
           <li class="list-group-item cet-footer-li">Nous sommes une association <b>Loi 1901</b></li>
           <li class="list-group-item cet-footer-li">À Castillon-la-Bataille : une épicerie bio coopérative, un café associatif, un espace de création et de travail partagé.</li>
@@ -25,7 +25,7 @@
           </li>
         </ul>
       </div>
-      <div class="col-6"> <!-- d-none d-lg-block -->
+      <div class="col-12 col-xl-4"> <!-- d-none d-lg-block -->
         <p>Pour venir nous rendre visite, c'est par ici :</p>
         <div class="iframe-container">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2828.4437754150376!2d-0.047651585114867875!3d44.8532597820216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd55577ce776d3ab%3A0x71a172a059d796a8!2s6%20Espl.%20Marcel%20Jouanno%2C%2033350%20Castillon-la-Bataille!5e0!3m2!1sen!2sfr!4v1592223896668!5m2!1sen!2sfr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
