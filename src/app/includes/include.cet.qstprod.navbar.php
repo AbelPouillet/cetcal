58b3faab2de2da6e64c782ca8674865338b39494
paulbis <tfydRuB2QCh33yH>
2021-08-09 11:47:29 +0200

    images header position

--- src/app/includes/include.cet.qstprod.navbar.php
+++ src/app/includes/include.cet.qstprod.navbar.php
@@ -1,35 +1,35 @@
 <header>
     <!--- DEBUT HEADER --->
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 herosection-wrapper d-lg-flex d-xl-flex flex-column-reverse flex-lg-row-reverse flex-xl-row-reverse">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 herosection-wrapper d-lg-flex d-xl-flex flex-column-reverse flex-lg-row-reverse flex-xl-row align-items-xl-center">
 
-                <div class="col-12 col-md-12 col-lg-3 col-xl-3 d-flex justify-content-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center">
+<!--                <div class="col-12 col-md-12 col-lg-3 col-xl-3 d-flex justify-content-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center">
 
-            <!--        <ul class="d-flex flex-lg-column flex-xl-column button-bag justify-content-around">
+                    <ul class="d-flex flex-lg-column flex-xl-column button-bag justify-content-around">
                         <li class="text-xl-center btnconnect" ><a class="btn--header" href="">Se connecter</a></li>
                         <li class="btnprd" ><a class="btn--header" href="">Je suis producteur</a></li>
                         <li class="btncustomer" ><a class="btn--header"href="">Je suis consommateur</a></li>
-                    </ul>-->
+                    </ul>
 
-                    <!--<div class="container-burger d-flex flex-xl-column align-items-xl-center">
+                    <div class="container-burger d-flex flex-xl-column align-items-xl-center">
                         <div class="bar1"></div>
                         <div class="bar2"></div>
                         <div class="bar3"></div>
-                    </div>-->
-                </div>
+                    </div>
+                </div>-->
 
-                <div class="col-12 col-sm-12 col-md-12 col-lg-5  col-xl-5">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-6">
                     <img class="w-100 mw-100" src="res/content/images-site/decidelabiolocale.png" alt="Max-width 100%"/>
                 </div>
 
-                <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-6">
                     <img class="w-100 mw-100" src="res/content/images-site/sous-titres.png" alt="Max-width 100%"/>
                 </div>
 
             </div>
         </div>
     <div class="row yellowLine d-block d-sm-none d-md-none d-lg-none d-xl-none"></div>
-    <div class="row pt-sm-5 pt-md-5 pt-lg-5 pt-xl-5 d-none d-sm-block d-md-block d-lg-block d-xl-block">
+    <div class="row d-none d-sm-block d-md-block d-lg-block d-xl-block">
             <!--DEBUT NAV MENU-->
             <div class="col-12 nav-wrapper d-flex">
                 <nav class="col-12 d-flex">
