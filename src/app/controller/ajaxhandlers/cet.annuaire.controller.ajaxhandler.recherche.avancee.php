2722848b2d3bc6ecee22d8dcfa76c88f3b2a0fc4
thw <thw@thw-a2>
2021-09-22 21:51:08 +0200

    racherche avancée : ajax

--- src/app/controller/ajaxhandlers/cet.annuaire.controller.ajaxhandler.recherche.avancee.php
+++ src/app/controller/ajaxhandlers/cet.annuaire.controller.ajaxhandler.recherche.avancee.php
@@ -2,4 +2,5 @@
 $DOC_ROOT = $_SERVER['DOCUMENT_ROOT'];
 require_once($DOC_ROOT.'/src/app/utils/cet.qstprod.utils.httpdataprocessor.php');
 $dataProcessor = new HTTPDataProcessor();
-error_log($_GET['json']);
+$json = json_decode($_GET['json']);
+error_log(print_r($json, true));
