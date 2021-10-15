f44b78a60ce20d1517d9a9dd1d801334bdb623c9
paulbis <tfydRuB2QCh33yH>
2021-10-15 17:11:18 +0200

    modifications visuelles diverses

--- src/app/includes/homepage/include.cet.annuaire.slogan.php
+++ src/app/includes/homepage/include.cet.annuaire.slogan.php
@@ -2,10 +2,10 @@
         <div class="row d-flex flex-column flex-xl-row slogan pb-2">
 
             <div class="col-12 d-flex flex-column justify-content-around flex-lg-row flex-xl-row">
-                <div class="d-flex justify-content-center align-items-center col-12 col-lg-4 col-xl-4">
+                <div class="d-flex justify-content-center align-items-center col-12 col-lg-4 col-xl-4 mb-2">
                     <img class="img-remettre" src="res/documentation/images-site/au-coeur.png" alt="Max-width 100%">
                 </div>
-                <div class="col-12 col-lg-4 col-xl-2 d-flex justify-content-center align-items-center p-xl-0">
+                <div class="col-12 col-lg-4 col-xl-2 d-flex justify-content-center align-items-center p-xl-0 mb-2">
                     <img class="img-abeu" src="res/content/icons/logos-verts-europe-ab.png" alt="Max-width 100%">
                 </div>
                 <div class="col-12 col-lg-4 col-xl-5 text-slogan-wrapper d-xl-flex flex-xl-column align-items-center p-xl-0 pr-xl-2">
