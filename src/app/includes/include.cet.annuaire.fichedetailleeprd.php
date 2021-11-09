2cf8abfbc00aeefd0ab05a83ff5b6a9ec859b058
thw <thw@thw-a2>
2021-11-09 20:44:54 +0100

    fiche producteur pour débloquer la prod.

--- src/app/includes/include.cet.annuaire.fichedetailleeprd.php
+++ src/app/includes/include.cet.annuaire.fichedetailleeprd.php
@@ -9,149 +9,85 @@ $media_controller = new MediaController();
 $certif_controller = new CertificationBioABProducteurController();
 $data = $controller->fetchProducteurByPk($pk);
 $certif_bioab = $certif_controller->getCertificationProducteur($pk);
-
-$produits = $controller->fetchProduitByPkProducteur($pk);
-$productsCategories = $controller->fetchCategorieProduitByPkProducteur($pk);
-$lieux = $controller->fetchAllLieuDistByPkProducteur($pk);
 ?>
-<div class="container-fluid ficheprd__wrapper">
-
-    <div class="container d-flex justify-content-center">
-        <!--debut wrapper-->
-        <div class="wrapper__ficheprd">
-            <section>
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-center p-0">
-                        <img class="w-75 w-xl-auto mw-100" src="res/documentation/images-site/qui-sommes-nous.png" alt="Max-width 100%" >
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div class="row">
-                    <div class="col-12">
-                        <!--logos-->
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-center p-0">
-                        <div class="card__producteur d-flex justify-content-center align-items-center">
-                            <h2 class="text-center"><?= $data['nom_ferme'] ?></h2>
-                        </div>
-                        <div class="card__producteur">
-                            <?php $media_data = $media_controller->selectMediasProducteur($pk); $counter = 0; ?>
-                            <div id="fichedetailleeprd-carousel" class="carousel slide" data-ride="carousel">
-                                <div class="carousel-inner">
-                                    <?php foreach ($media_data as $media): ?>
-                                        <div class="carousel-item <?= $counter == 0 ? 'active' : $neant; ?>"
-                                             style="">
-                                            <img class="img__producteur" src="<?= $media['urlr']; ?>" style="" />
-                                        </div>
-                                        <?php ++$counter; ?>
-                                    <?php endforeach; ?>
-                                    <?php if ($counter == 0): ?>
-                                        <div class="carousel-item active" style="">
-                                            <img class="img__producteur" src="/res/content/DCDL_biolocale_1.jpg" style="" />
-                                        </div>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-            </section>
-            <section hidden="hidden">
-                <div class="row">
-                    <div class="col-12 d-flex align-items-center justify-content-center p-0">
-                        <!--texte-->
-                        <div class="d-flex align-items-center text__presentation">
-                            <p class="text-center">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda atque autem distinctio ea error, et eum fugit hic illo ipsam odio, provident quod rem repellat repellendus totam voluptates. Iste!
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
-            </section>
-
-            <section class="d-flex justify-content-center">
-                <div class="wrapper_infoprd">
-                    <div class="row">
-                        <div class="col-12 d-flex">
-                            <div class="col-6 card__coordonnees">
-                                <!-- coordonnées-->
-                                <h3>Coordonnées</h3>
-                                <p><?= ucfirst($data['prenom']); ?> <?= ucfirst($data['nom']); ?></p>
-                                <address>
-                                    <?php
-                                    $adr = str_replace("  ", " ", $data['adrferme_numvoie'].' '.$data['adrferme_rue'].' '
-                                        .$data['adrferme_lieudit'].' '.$data['adrferme_commune'].' '
-                                        .$data['adrferme_cp'].' '.$data['adrferme_compladr']);
-                                    ?>
-                                    <p><?= $adr?></p>
-                                    <?=  (isset($data["telfixe"]))  ?  "<p>".$data['telfixe'] . "</p>" : "" ?>
-                                    <?=  (isset($data["telport"]))  ?  "<p>".$data['telport'] . "</p>" : "" ?>
-                                    <?=  (isset($data["email"]))  ?  "<p class='card_mail'>".$data['email'] . "</p>" : "" ?>
-                                </address>
-                                <div>
-                                    <?=  (strlen($data["url_web"]) > 0)  ?  "<a class='cst-pills' href=".$data["url_web"].">"."site web" . "</a>" : "" ?>
-                                    <?=  (strlen($data["pageurl_fb"]) > 0)  ?  "<a class='cst-pills' href=".$data["pageurl_fb"].">"."facebook" . "</a>" : "" ?>
-
-                                </div>
-                            </div>
-                            <div class="col-6 card__production">
-                                <!--production-->
-                                <h3>Production</h3>
-                                <div>
-                                    <?php foreach ( $produits as $produit) : ?>
-                                        <span class="cst-pills"><?=  $produit->nom ?></span>
-                                    <?php endforeach; ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 d-flex">
-                            <div class="col-6 card__autre">
-                                <!--ouverture au public-->
-                            </div>
-                            <div class="col-6 card__lieuxvente">
-                                <!--Lieux de vente-->
-                                <h3>Lieux de vente</h3>
-                                <div>
-                                    <?php foreach ($lieux as $lieu): ?>
-                                        <div class="lieux-vente-producteur">
-                                            <p><?= $lieu->denomination?></p>
-                                            <p><?= $lieu->precisions?></p>
-                                        </div>
-                                    <?php endforeach;?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end of row-->
-
-                </div>
-            </section>
-            <?php if($certif_bioab != false): ?>
-                <section>
-                    <div class="row">
-                        <div class="col-12 p-0">
-                            <div class="wrapper__certif ">
-                                <!--logos AB-->
-                                <div class="d-flex align-items-center">
-                                    <img src="/res/content/icons/logos-verts-europe-ab.png">
-                                    <a href="<?= $certif_bioab['url_org_certif']; ?>">consulter la certification bio AB</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--end of row-->
-                    </div>
-                </section>
-            <?php endif;?>
+<br><br>
+<div class="container">
+  <div class="row d-flex justify-content-center flex-column-reverse flex-md-row flex-xl-row">
+    <div class="col-xs-12 col-md-8 col-xl-4">
+      <div class="cet-formgroup-container" style="overflow-wrap: break-word;">
+        <h4 id="fichedetailleeprd-nom-ferme"><?= ucfirst($data['nom_ferme']); ?></h4>
+        <?php if (isset($certif_bioab) && $certif_bioab !== false && strlen($certif_bioab['url_org_certif']) > 7): ?> 
+          <div class="row justify-content-center" style="margin-bottom: 12px;">
+            <div class="col-8">
+              <a href="<?= $certif_bioab['url_org_certif']; ?>" target="_blank">
+                <img class="img-fluid" src="/res/content/icons/logos-verts-europe-ab.png"/>
+              </a>
+            </div>
+          </div>
+        <?php endif; ?>
+        <?php $logo_ferme = $media_controller->selectSrcLogoFemreProducteur($pk); ?>
+        <?php if (isset($logo_ferme) && strlen($logo_ferme) > 1): ?> 
+          <p>
+            <img class="fichedetailleeprd-logo-ferme img-fluid" src="<?= $logo_ferme; ?>"/>
+          </p>
+        <?php endif; ?>
+        <p>
+          <span><?= ucfirst($data['prenom']); ?> <?= ucfirst($data['nom']); ?></span><br>
+          <?php
+            $adr = str_replace("  ", " ", $data['adrferme_numvoie'].' '.$data['adrferme_rue'].' '
+              .$data['adrferme_lieudit'].' '.$data['adrferme_commune'].' '
+              .$data['adrferme_cp'].' '.$data['adrferme_compladr']);
+          ?>
+          <span><?= $adr; ?></span><br>
+          <span>Tél : <?= $data['telfixe']; ?></span><br>
+          <span>Tél mobile : <?= $data['telport']; ?></span><br>
+          <span>Email : <?= $data['email']; ?></span><br>
+          <span>Site web : <br>
+            <a href="<?= $data['url_web']; ?>" target="_blank"><?= $data['url_web']; ?></a>
+          </span><br>
+          <span>Boutique en ligne : <br>
+            <a href="<?= $data['url_boutique']; ?>" target="_blank"><?= $data['url_boutique']; ?></a>
+          </span><br>
+          <?php if (isset($data['pageurl_fb']) && strlen($data['pageurl_fb']) > 3): ?>
+            <span>
+              <a href="<?= $data['pageurl_fb']; ?>" target="_blank"><img class="cet-crt-icon" src="/res/content/icons/facebook-flat-icon.png" height="42"/></a>
+            </span>
+          <?php endif; ?>
+          <?php if (isset($data['pageurl_ig']) && strlen($data['pageurl_ig']) > 3): ?>
+            <span>
+              <a href="<?= $data['pageurl_ig']; ?>" target="_blank"><img class="cet-crt-icon" src="/res/content/icons/ig-flat-icon_256.png" height="42"/></a>
+            </span>
+          <?php endif; ?>
+          <?php if (isset($certif_bioab) && $certif_bioab !== false && strlen($certif_bioab['url_org_certif']) > 7): ?>
+            <a class="btn btn-small btn-outline-success" href="<?= $certif_bioab['url_org_certif']; ?>" target="_blank" style="margin-top: 12px;">
+              <b><i class="fas fa-stamp"></i>&#160;&#160;Consulter la certification BIO/AB de <?= ucfirst($data['nom_ferme']); ?></b>
+            </a>
+          <?php endif; ?>
+        </p>
+      </div>
+    </div>
+    <div class="col-xs-12 col-md-4 col-xl-4">
+      <div style="overflow-wrap: break-word; margin-bottom: 16px !important;">
+        <?php $media_data = $media_controller->selectMediasProducteur($pk); $counter = 0; ?>
+        <div id="fichedetailleeprd-carousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <?php foreach ($media_data as $media): ?>
+              <div class="carousel-item <?= $counter == 0 ? 'active' : $neant; ?>"
+                style="">
+                <img class="img-fluid" src="<?= $media['urlr']; ?>" style="" />
+              </div>
+              <?php ++$counter; ?>
+            <?php endforeach; ?>  
+            <?php if ($counter == 0): ?>
+              <div class="carousel-item active" style="">
+                <img class="img-fluid" src="/res/content/DCDL_biolocale_1.jpg" style="" />
+              </div>
+            <?php endif; ?>
+          </div>
         </div>
-        </div>
-        <!--Fin wrapper-->
-
+      </div>
+    </div>
+  </div>
 </div>
-
-
+<?php include $PHP_INCLUDES_PATH.'/areas/include.cet.annuaire.produits.fichedetailleeprd.php'; ?>
+<script src="/src/scripts/js/cetcal/cetcal.min.fichedetailleeprd.js"></script>
