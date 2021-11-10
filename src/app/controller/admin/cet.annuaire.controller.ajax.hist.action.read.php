327eb2d43a4dd8831477c5a7a89c25712172d14c
thw <thw@thw-a2>
2021-11-10 22:03:45 +0100

    update

--- src/app/controller/admin/cet.annuaire.controller.ajax.hist.action.read.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/src/app/model/cet.cetcal.administrateur.hist.action.model.php');
-$mdl = new CETCALAdminHistoriqueActionModel();
-$data = $mdl->getAll();
-echo json_encode($data);
-exit();
